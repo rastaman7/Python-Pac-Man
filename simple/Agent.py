280908d550d529fbeec01172abd1555c44fc3b0b
rastaman7 <34154020+rastaman7@users.noreply.github.com>
2018-10-15 15:06:47 +0900

    Update Agent.py

--- simple/Agent.py
+++ simple/Agent.py
@@ -1,4 +1,4 @@
-class Agent:
+ Agent:
     def __init__(self, grid, row, col):
         self.grid=grid
         self.row=row
@@ -7,7 +7,7 @@ class Agent:
         self.grid.cells[row][col]=1
 
     def move_up(self):
-        if(self.row>0):
+        if self.row>0 and self.grid.cells[self.row-1][self.col] != 1:
             self.row=self.row-1
             self.grid.cells[self.row][self.col]=1
             self.grid.cells[self.row+1][self.col]=0
@@ -15,7 +15,7 @@ class Agent:
             print("unable to move up")
 
     def move_down(self):
-        if(self.row<self.grid.row_num-1):
+        if self.row<self.grid.row_num-1 and self.grid.cells[self.row+1][self.col] != 1:
             self.row=self.row+1
             self.grid.cells[self.row][self.col]=1
             self.grid.cells[self.row-1][self.col]=0
@@ -23,15 +23,15 @@ class Agent:
             print("unable to move down")
 
     def move_left(self):
-        if(self.col>0):
+        if self.col>0 and self.grid.cells[self.row][self.col-1] != 1:
             self.col=self.col-1
             self.grid.cells[self.row][self.col]=1
-            self.grid.cells[self.row][self.col+1]=0
+           class self.grid.cells[self.row][self.col+1]=0
         else:
             print("unable to move left")
 
     def move_right(self):
-        if(self.col<self.grid.col_num-1):
+        if self.col<self.grid.col_num-1 and self.grid.cells[self.row][self.col+1] != 1:
             self.col=self.col+1
             self.grid.cells[self.row][self.col]=1
             self.grid.cells[self.row][self.col-1]=0
