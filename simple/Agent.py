61329aa4551da115dd71251d3804272161ef8995
rastaman7 <34154020+rastaman7@users.noreply.github.com>
2018-10-15 12:58:40 +0900

    Create Agent.py

---
+++ simple/Agent.py
@@ -0,0 +1,39 @@
+class Agent:
+    def __init__(self, grid, row, col):
+        self.grid=grid
+        self.row=row
+        self.col=col
+
+        self.grid.cells[row][col]=1
+
+    def move_up(self):
+        if(self.row>0):
+            self.row=self.row-1
+            self.grid.cells[self.row][self.col]=1
+            self.grid.cells[self.row+1][self.col]=0
+        else:
+            print("unable to move up")
+
+    def move_down(self):
+        if(self.row<self.grid.row_num-1):
+            self.row=self.row+1
+            self.grid.cells[self.row][self.col]=1
+            self.grid.cells[self.row-1][self.col]=0
+        else:
+            print("unable to move down")
+
+    def move_left(self):
+        if(self.col>0):
+            self.col=self.col-1
+            self.grid.cells[self.row][self.col]=1
+            self.grid.cells[self.row][self.col+1]=0
+        else:
+            print("unable to move left")
+
+    def move_right(self):
+        if(self.col<self.grid.col_num-1):
+            self.col=self.col+1
+            self.grid.cells[self.row][self.col]=1
+            self.grid.cells[self.row][self.col-1]=0
+        else:
+            print("unable to move right")
