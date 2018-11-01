eedb066e78b9065508c05476361e3b0b111ac14d
rastaman7 <34154020+rastaman7@users.noreply.github.com>
2018-11-02 01:29:58 +0900

    Update Agent.py

--- simple/Agent.py
+++ simple/Agent.py
@@ -1,10 +1,22 @@
- Agent:
+ class Agent:
+
+    new_id = -1
+    AgentList=[]
+
+    @staticmethod
+    def __getNewID():
+        Agent.new_id += 1
+        return Agent.new_id
+
     def __init__(self, grid, row, col):
+        self.id = Agent.__getNewID()
         self.grid=grid
         self.row=row
         self.col=col
+        self.score=0
 
         self.grid.cells[row][col]=1
+        Agent.AgentList.append(self)
 
     def move_up(self):
         if self.row>0 and self.grid.cells[self.row-1][self.col] != 1:
@@ -26,7 +38,7 @@
         if self.col>0 and self.grid.cells[self.row][self.col-1] != 1:
             self.col=self.col-1
             self.grid.cells[self.row][self.col]=1
-           class self.grid.cells[self.row][self.col+1]=0
+            self.grid.cells[self.row][self.col+1]=0
         else:
             print("unable to move left")
 
@@ -37,3 +49,9 @@
             self.grid.cells[self.row][self.col-1]=0
         else:
             print("unable to move right")
+
+    def earn_score(self):
+        self.score=self.score+1
+
+    def get_score(self):
+        return self.score
