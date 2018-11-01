6aa7991e6d27f48c89e5f10632eea7b25e02bebc
rastaman7 <34154020+rastaman7@users.noreply.github.com>
2018-11-02 01:27:54 +0900

    Update app.py

--- simple/app.py
+++ simple/app.py
@@ -1,26 +1,42 @@
-from Grid import Grid
+ from Grid import Grid
 from Agent import Agent
+from Task import Task
 
-grid1 = Grid(4,4)
-agent1=Agent(grid1,2,2)
+grid1 = Grid(5,5)
+agent1=Agent(grid1,3,3)
+agent2=Agent(grid1,1,1)
+item1=Task(grid1,2,2)
 
 Grid.printGrid(grid1)
 
 while True:
-    x=input()
-    if(x=='q'):
+    n=input()
+
+    if(n=='q'):
         break
-    elif(x=='u'):
-        agent1.move_up()
-        Grid.printGrid(grid1)
-    elif(x=='d'):
-        agent1.move_down()
-        Grid.printGrid(grid1)
-    elif(x=='l'):
-        agent1.move_left()
-        Grid.printGrid(grid1)
-    elif(x=='r'):
-        agent1.move_right()
-        Grid.printGrid(grid1)
+    elif(int(n)<=Agent.new_id and int(n)>=0):
+        for a in Agent.AgentList:
+            if(a.id==int(n)):
+                x=input()
+                if(x=='q'):
+                    print(a.score)
+                    break
+                elif(x=='u'):
+                    a.move_up()
+                    Grid.printGrid(grid1)
+                elif(x=='d'):
+                    a.move_down()
+                    Grid.printGrid(grid1)
+                elif(x=='l'):
+                    a.move_left()
+                    Grid.printGrid(grid1)
+                elif(x=='r'):
+                    a.move_right()
+                    Grid.printGrid(grid1)
+                else:
+                    print("Wrong command. Try again.")
+            else:
+                print("haha")
+
     else:
         print("Wrong command. Try again.")
