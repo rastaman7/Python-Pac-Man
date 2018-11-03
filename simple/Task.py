8056e6dad154e5bdc660ed336bdf7a551bab00ae
rastaman7 <34154020+rastaman7@users.noreply.github.com>
2018-11-03 13:05:13 +0900

    Update Task.py

--- simple/Task.py
+++ simple/Task.py
@@ -1,10 +1,21 @@
+from TaskList import TaskList
+
 class Task:
+    new_id = -1
+
+    @staticmethod
+    def __getNewID():
+        Task.new_id += 1
+        return Task.new_id
+
     def __init__(self, grid, row, col):
+        self.id = Task.__getNewID()
         self.grid=grid
         self.row=row
         self.col=col
 
-        self.grid.cells[row][col]=2
+        self.grid.cells[row][col]=1
+        TaskList(self)
 
     def removeItem(self):
         self.grid.cells[self.row][self.col]=0
