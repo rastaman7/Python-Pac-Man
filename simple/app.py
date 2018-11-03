41ecb72a52cb2b7f82f9e2b093c474d3a278de71
rastaman7 <34154020+rastaman7@users.noreply.github.com>
2018-11-03 13:04:07 +0900

    Update app.py

--- simple/app.py
+++ simple/app.py
@@ -1,13 +1,16 @@
- from Grid import Grid
+from Grid import Grid
 from Agent import Agent
 from Task import Task
+from TaskList import TaskList
 
 grid1 = Grid(5,5)
+grid2 = Grid(5,5)
 agent1=Agent(grid1,3,3)
 agent2=Agent(grid1,1,1)
-item1=Task(grid1,2,2)
+item1=Task(grid2,2,2)
 
 Grid.printGrid(grid1)
+Grid.printGrid(grid2)
 
 while True:
     n=input()
@@ -19,24 +22,27 @@ while True:
             if(a.id==int(n)):
                 x=input()
                 if(x=='q'):
+                    print("Wrong Command")
                     print(a.score)
                     break
                 elif(x=='u'):
                     a.move_up()
                     Grid.printGrid(grid1)
+                    Grid.printGrid(grid2)
                 elif(x=='d'):
                     a.move_down()
                     Grid.printGrid(grid1)
+                    Grid.printGrid(grid2)
                 elif(x=='l'):
                     a.move_left()
                     Grid.printGrid(grid1)
+                    Grid.printGrid(grid2)
                 elif(x=='r'):
                     a.move_right()
                     Grid.printGrid(grid1)
+                    Grid.printGrid(grid2)
                 else:
                     print("Wrong command. Try again.")
-            else:
-                print("haha")
 
     else:
         print("Wrong command. Try again.")
