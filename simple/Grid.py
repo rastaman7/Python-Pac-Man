ad47c32a23bf80afcce1ef9473cefacf9dbd37e1
rastaman7 <34154020+rastaman7@users.noreply.github.com>
2018-11-13 12:36:15 +0900

    Update Grid.py

--- simple/Grid.py
+++ simple/Grid.py
@@ -1,13 +1,15 @@
 class Grid:
-    def __init__(self, row_num, col_num):
-        self.row_num=row_num
-        self.col_num=col_num
+    row_num=6
+    col_num=6
+    def __init__(self):
+        self.row_num=Grid.row_num
+        self.col_num=Grid.col_num
         self.cells = []
 
-        for i in range(row_num):
-            self.cells.append([0 for i in range(col_num)])
+        for i in range(Grid.row_num):
+            self.cells.append([0 for i in range(Grid.col_num)])
 
     def printGrid(self):
-        for i in range(self.row_num):
+        for i in range(Grid.row_num):
             print(self.cells[i])
-        print("------------")
+        print("--------------")
