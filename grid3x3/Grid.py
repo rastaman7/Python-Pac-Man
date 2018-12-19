3f9c43b8ff51eb57e69599ba1fc64946e09a6af7
rastaman7 <34154020+rastaman7@users.noreply.github.com>
2018-12-19 11:08:23 +0900

    Create Grid.py

---
+++ grid3x3/Grid.py
@@ -0,0 +1,15 @@
+class Grid:
+    row_num=3
+    col_num=3
+    def __init__(self):
+        self.row_num=Grid.row_num
+        self.col_num=Grid.col_num
+        self.cells = []
+
+        for i in range(Grid.row_num):
+            self.cells.append([0 for i in range(Grid.col_num)])
+
+    def printGrid(self):
+        for i in range(Grid.row_num):
+            print(self.cells[i])
+        print("--------------")
