87f5ebdb1fe8c828a225ffdfa3e017dabf889158
rastaman7 <34154020+rastaman7@users.noreply.github.com>
2018-12-19 11:08:52 +0900

    Create Score.py

---
+++ grid3x3/Score.py
@@ -0,0 +1,13 @@
+from Agent import Agent
+from Task import Task
+from TaskList import TaskList
+
+class Score:
+
+    def checkScore(agent, taskgrid):
+        if taskgrid.cells[agent.row][agent.col]==1:
+            for b in TaskList.TaskList:
+                if b.row==agent.row and b.col==agent.col:
+                    agent.earn_score()
+                    b.removeTask()
+                    Task.crateTask(agent.grid, taskgrid)
