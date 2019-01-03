80da0de03c0e7d1e99726aff36bb3ee744213466
rastaman7 <34154020+rastaman7@users.noreply.github.com>
2019-01-03 20:27:53 +0900

    Update Agent.py

--- simple/Agent.py
+++ simple/Agent.py
@@ -5,45 +5,44 @@ class Agent:
 
     new_id = -1
     AgentList=[]
+    view_row=3
+    view_col=3
+    view_row_mid=int((view_row-1)/2)
+    view_col_mid=int((view_col-1)/2)
 
     @staticmethod
     def __getNewID():
         Agent.new_id += 1
         return Agent.new_id
 
-    def agentView():
+
+    def agentView7():
         for a in Agent.AgentList:
-            a.agentview=[[0,0,0],[0,0,0],[0,0,0]]
-            a.taskview=[[0,0,0],[0,0,0],[0,0,0]]
-            if a.row==0:
-                a.agentview[0]=[-1,-1,-1]
-                a.taskview[0]=[-1,-1,-1]
-            if a.row==Grid.row_num-1:
-                a.agentview[2]=[-1,-1,-1]
-                a.taskview[2]=[-1,-1,-1]
-            if a.col==0:
-                a.agentview[0][0]=-1
-                a.agentview[1][0]=-1
-                a.agentview[2][0]=-1
-                a.taskview[0][0]=-1
-                a.taskview[1][0]=-1
-                a.taskview[2][0]=-1
-            if a.col==Grid.col_num-1:
-                a.agentview[0][2]=-1
-                a.agentview[1][2]=-1
-                a.agentview[2][2]=-1
-                a.taskview[0][2]=-1
-                a.taskview[1][2]=-1
-                a.taskview[2][2]=-1
-            for b in Agent.AgentList:
-                if b.row-a.row+1>=0 and b.row-a.row+1<=2:
-                    if b.col-a.col+1>=0 and b.col-a.col+1<=2:
-                        if a.row!=b.row or a.col!=b.col:
-                            a.agentview[b.row-a.row+1][b.col-a.col+1]=1
-            for c in TaskList.TaskList:
-                if c.row-a.row+1>=0 and c.row-a.row+1<=2:
-                    if c.col-a.col+1>=0 and c.col-a.col+1<=2:
-                        a.taskview[c.row-a.row+1][c.col-a.col+1]=1
+            a.agentview=[]
+            a.taskview=[]
+
+            for i in range(Agent.view_row):
+                a.agentview.append([0 for i in range(Agent.view_col)])
+                a.taskview.append([0 for i in range(Agent.view_col)])
+
+            for i in range(Agent.view_row):
+                for j in range(Agent.view_col):
+                    if a.row-(Agent.view_row_mid-i)<0 or a.row-(Agent.view_row_mid-i)>Grid.row_num-1:
+                        a.agentview[i][j]=-1
+                        a.taskview[i][j]=-1
+                    elif a.col-(Agent.view_col_mid-j)<0 or a.col-(Agent.view_col_mid-j)>Grid.col_num-1:
+                        a.agentview[i][j]=-1
+                        a.taskview[i][j]=-1
+                    else:
+                        for b in Agent.AgentList:
+                            if b.row==a.row-(Agent.view_row_mid-i) and b.col==a.col-(Agent.view_col_mid-j):
+                                a.agentview[i][j]=1
+                        for c in TaskList.TaskList:
+                            if c.row==a.row-(Agent.view_row_mid-i) and c.col==a.col-(Agent.view_col_mid-j):
+                                a.taskview[i][j]=1
+
+
+
 
     def __init__(self, grid, row, col):
         self.id = Agent.__getNewID()
@@ -51,8 +50,12 @@ class Agent:
         self.row=row
         self.col=col
         self.score=0
-        self.agentview=[[0,0,0],[0,0,0],[0,0,0]]
-        self.taskview=[[0,0,0],[0,0,0],[0,0,0]]
+        self.agentview=[]
+        self.taskview=[]
+
+        for i in range(Agent.view_row):
+            self.agentview.append([0 for i in range(Agent.view_col)])
+            self.taskview.append([0 for i in range(Agent.view_col)])
 
         self.grid.cells[row][col]=1
         Agent.AgentList.append(self)
@@ -62,16 +65,16 @@ class Agent:
             self.row=self.row-1
             self.grid.cells[self.row][self.col]=1
             self.grid.cells[self.row+1][self.col]=0
-        else:
-            print("unable to move up")
+        #else:
+            #print("unable to move up")
 
     def move_down(self):
         if self.row<self.grid.row_num-1 and self.grid.cells[self.row+1][self.col] != 1:
             self.row=self.row+1
             self.grid.cells[self.row][self.col]=1
             self.grid.cells[self.row-1][self.col]=0
-        else:
-            print("unable to move down")
+        #else:
+            #print("unable to move down")
 
     def move_left(self):
         if self.col>0 and self.grid.cells[self.row][self.col-1] != 1:
@@ -91,7 +94,6 @@ class Agent:
 
     def earn_score(self):
         self.score=self.score+1
-
     def get_score(self):
         return self.score
 
@@ -104,3 +106,12 @@ class Agent:
         for i in range(3):
             print(self.taskview[i])
         print("-------")
+
+    def moveAgent(self, newrow, newcol):
+        self.row=newrow
+        self.col=newcol
+
+    def removeAgent(self):
+        self.grid.cells[self.row][self.col]=0
+        Agent.AgentList.remove(self)
+        del self
